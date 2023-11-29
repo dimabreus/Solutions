2db42ee72547e902cf77ddb33e3e0d304184c2cd
dimabreus <dmitriybreus5@gmail.com>
2023-11-29 18:45:13 +0300

    Edited turtle-game

--- turtle-game.py
+++ turtle-game.py
@@ -1,22 +1,29 @@
-from turtle import *
+from turtle import RawTurtle, ScrolledCanvas
 import tkinter as tk
-
-bgcolor("lightblue")
-shape("turtle")
 
 window = tk.Tk()
 window.title("Turtle управление")
 window.geometry("800x800")
 window.configure(bg="#7a7a7a")
 
-forward_btn = tk.Button(window, text="forward", command=lambda: forward(100), bg="#5e5e5e")
-back_btn = tk.Button(window, text="back", command=lambda: back(100), bg="#5e5e5e")
-left_btn = tk.Button(window, text="left", command=lambda: left(90), bg="#5e5e5e")
-right_btn = tk.Button(window, text="right", command=lambda: right(100), bg="#5e5e5e")
+forward_btn = tk.Button(window, text="forward", command=lambda: turtle.forward(100), bg="#5e5e5e")
+back_btn = tk.Button(window, text="back", command=lambda: turtle.back(100), bg="#5e5e5e")
+left_btn = tk.Button(window, text="left", command=lambda: turtle.left(90), bg="#5e5e5e")
+right_btn = tk.Button(window, text="right", command=lambda: turtle.right(90), bg="#5e5e5e")
+
+canvas_frame = tk.Frame(window)
+canvas_frame.pack(expand=True, fill=tk.BOTH)
+
+canvas = ScrolledCanvas(canvas_frame)
+canvas.pack(expand=True, fill=tk.BOTH)
+
+turtle = RawTurtle(canvas)
+turtle_screen = turtle.getscreen()
+turtle_screen.bgcolor("white")
 
 forward_btn.pack()
 back_btn.pack()
 left_btn.pack()
 right_btn.pack()
 
-mainloop()
+window.mainloop()
