fac2d1d0b2dcf6c51399fdea0451daefa1619239
dimabreus <dmitriybreus5@gmail.com>
2023-12-29 19:30:40 +0300

    a

--- Library/rectangle_circle.py
+++ Library/rectangle_circle.py
@@ -7,3 +7,7 @@ def rectangle_circle(a: float, b: float, circles: int):
     for i in range(circles):
         rectangle(a, b)
         left(360 / circles)
+
+speed(1000)
+rectangle_circle(100, 50, 16)
+mainloop()
